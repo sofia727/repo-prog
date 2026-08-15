0fbe9f8c5635446bfab1f7c698c8215e5559123c
sofia <sofiademello33@gmail.com>
2026-08-15 13:14:35 -0300

    primeiro commit

---
+++ fmto_Scanner1/ex11.java
@@ -0,0 +1,15 @@
+package fmto_Scanner1;
+
+public class ex11 {
+	public static void main(String[] args) {
+
+		int nota = 6;
+		int frequencia = 75;
+
+		String resultado = (nota >= 7 && frequencia >= 75) ? "Aprovado" : "Reprovado";
+
+		System.out.println(resultado);
+
+	}
+
+}
